64a624bf88656d57675756a6699eae0cba8a9d33
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-14 22:15:04 +0530

    Time: 60 ms (32.44%) | Memory: 16.4 MB (5.20%) - LeetSync

---
+++ next-permutation/next-permutation.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k, l = n - 2, n - 1
+        while k >= 0 and nums[k] >= nums[k + 1]:
+            k -= 1
+        if k < 0:
+            nums.reverse()
+        else:
+            while l > k and nums[l] <= nums[k]:
+                l -= 1
+            nums[k], nums[l] = nums[l], nums[k]
+            nums[k + 1:n] = reversed(nums[k + 1:n])
+                
