35c798bec55f9e4986ec812a1d26061786e6981f
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-14 22:29:53 +0530

    Time: 59 ms (39.20%) | Memory: 16.1 MB (80.77%) - LeetSync

--- next-permutation/next-permutation.py
+++ next-permutation/next-permutation.py
@@ -3,16 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        s=len(nums)-2
-        j=len(nums)-1
-        while s>=0 and nums[s]>=nums[s+1]:
-            s-=1
-        if s<0:
+        n = len(nums)
+        k, l = n - 2, n - 1
+        while k >= 0 and nums[k] >= nums[k + 1]:
+            k -= 1
+        if k < 0:
             nums.reverse()
-        else:   
-            while j>s and nums[j]<=nums[s]:
-                j-=1
-            nums[j],nums[s]=nums[s],nums[j]
-            nums[s+1:]=reversed(nums[s+1:])  
-                    
-                
+        else:
+            while l > k and nums[l] <= nums[k]:
+                l -= 1
+            nums[k], nums[l] = nums[l], nums[k]
+            nums[k + 1:n] = reversed(nums[k + 1:n])
