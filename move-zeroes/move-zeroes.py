17fc4dc246d2e5cc50b4e2deb567a3bd18a11362
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-17 21:27:57 +0530

    Time: 178 ms (56.83%) | Memory: 18 MB (9.81%) - LeetSync

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -3,9 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        start=0
+        end=len(nums)-1
+        mid=0
+        while(mid<=end):
+            if(nums[mid]!=0):
+                nums[start],nums[mid]=nums[mid],nums[start]                
+                start+=1
+            mid+=1
+
         
-        for i in range(len(nums)):
-            if nums[i]==0:
-                nums.remove(nums[i])
-                nums.append(0)
-        return nums
