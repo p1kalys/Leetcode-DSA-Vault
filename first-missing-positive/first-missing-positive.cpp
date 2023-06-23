add573d393cdf29c0baa67deb48bc09538ba6986
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-23 11:38:50 +0530

    Time: 131 ms (25.20%) | Memory: 57 MB (23.87%) - LeetSync

---
+++ first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        for(auto x: nums){
+            mp[x]++;
+        }
+        for(int i=1; i<=n+1; i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
