0067eeb9c242e27c2a3c4920841304e020430571
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-24 22:54:46 +0530

    Time: 560 ms (66.67%) | Memory: 16.7 MB (87.50%) - LeetSync

--- tallest-billboard/tallest-billboard.py
+++ tallest-billboard/tallest-billboard.py
@@ -1,17 +1,10 @@
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        _sum = sum(rods)
-        dp = [-1] * (_sum + 1)
-        dp[0] = 0
+        dp = {0:0}
 
-        for rod in rods:
-            dp_copy = dp[:]
-
-            for i in range(_sum - rod + 1):
-                if dp_copy[i] < 0:
-                    continue
-
-                dp[i + rod] = max(dp[i + rod], dp_copy[i])
-                dp[abs(i - rod)] = max(dp[abs(i - rod)], dp_copy[i] + min(i, rod))
+        for rod in rods: 
+            for diff, val in dp.copy().items(): 
+                dp[diff + rod] = max(dp.get(diff + rod, 0), val)
+                dp[abs(diff - rod)] = max(dp.get(abs(diff - rod), 0), val + min(rod, diff))
 
         return dp[0]
