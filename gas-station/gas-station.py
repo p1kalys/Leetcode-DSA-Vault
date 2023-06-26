def27ae79389847183483debcd1fe693c81a15d4
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-26 16:12:23 +0530

    Time: 1219 ms (41.27%) | Memory: 22.3 MB (48.80%) - LeetSync

--- gas-station/gas-station.py
+++ gas-station/gas-station.py
@@ -2,12 +2,12 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n=len(gas)
         Fuel=0
-        stops=0
+        stop_Fuel=0
         start=0
         for i in range(n):
             Fuel+=gas[i]-cost[i]
-            stops+=gas[i]-cost[i]
-            if stops<0:
-                stops=0
+            stop_Fuel+=gas[i]-cost[i]
+            if stop_Fuel<0:
+                stop_Fuel=0
                 start=i+1
         return -1 if Fuel<0 else start
