c830067ebdfba990efe21c89edd0f89e3fc2b0b6
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-27 23:17:59 +0530

    Time: 2 ms (47.55%) | Memory: 5.9 MB (93.36%) - LeetSync

--- factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int count = 0 ;
-        for (int i = 5; n / i >= 1; i *= 5){
-            count += n / i;
+        if (n == 0){
+            return 0;
         }
-        return count;
+        return n/5 + trailingZeroes(n/5);
         
     }
 };
