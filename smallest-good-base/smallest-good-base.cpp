71ae2653a622f7a54d56c36ab3bfe113ea779f52
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-06-28 11:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (84.56%) - LeetSync

--- smallest-good-base/smallest-good-base.cpp
+++ smallest-good-base/smallest-good-base.cpp
@@ -1,20 +1,40 @@
 class Solution {
- public:
-  string smallestGoodBase(string n) {
-    const long num = stol(n);
-
-    for (int m = log2(num); m >= 2; --m) {
-      const int k = pow(num, 1.0 / m);
-      long sum = 1;
-      long prod = 1;
-      for (int i = 0; i < m; ++i) {
-        prod *= k;
-        sum += prod;
-      }
-      if (sum == num)
-        return to_string(k);
+    unsigned long long mysolve(unsigned long long n, int d) {
+        double tn = (double)n;
+        unsigned long long right = (unsigned long long)(pow(tn, 1.0 / d) + 1);
+        unsigned long long left = 1;
+        while (left <= right)
+        {
+            unsigned long long mid = left + (right - left) / 2;
+            unsigned long long sum = 1, cur = 1;
+            for (int i = 1; i <= d; i++)
+            {
+                cur *= mid;
+                sum += cur;
+            }
+            if (sum == n)
+                return mid;
+            if (sum > n)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return 0;
     }
 
-    return to_string(num - 1);
-  }
+public:
+    string smallestGoodBase(string n) {
+        unsigned long long tn = (unsigned long long)stoll(n);
+        unsigned long long x = 1;
+        for (int i = 62; i >= 1; i--)
+        {
+            if ((x << i) < tn)
+            {
+                unsigned long long cur = mysolve(tn, i);
+                if (cur != 0)
+                    return to_string(cur);
+            }
+        }
+        return to_string(tn - 1);
+    }
 };
