d45be563d670fc9736d147604eeef838f8fab673
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-02 21:41:58 +0530

    Time: 46 ms (95.07%) | Memory: 16.3 MB (93.74%) - LeetSync

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,9 +1,15 @@
-from itertools import permutations
+def helper(index,nums,res):
+    if index==len(nums):
+        res.append(nums[:])
+        return
+    for i in range(index,len(nums)):
+        nums[i],nums[index]=nums[index],nums[i]
+        helper(index+1,nums,res)
+        nums[i],nums[index]=nums[index],nums[i]
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res=[]
-        permut=permutations(nums)
-        for i in list(sorted(set(permut))):
-            res.append(i)
+        helper(0,nums,res)
         return res
         
