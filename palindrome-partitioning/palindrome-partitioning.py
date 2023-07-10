8cfe2295de7b47d61599bf7c43b5b6b383e09eab
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-10 23:59:43 +0530

    Time: 686 ms (24.05%) | Memory: 30.2 MB (99.22%) - LeetSync

---
+++ palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,15 @@
+class Solution:
+    def dp(self,i,s,st):
+        if i>=len(s):
+            self.lst.append(tuple(st))
+            return 
+        for j in range(i,len(s)):
+            if s[i:j+1]==''.join(reversed(s[i:j+1])):
+                st.append(s[i:j+1])
+                x=self.dp(j+1,s,st)
+                st.pop()
+        return
+    def partition(self, s: str) -> List[List[str]]:
+        self.lst=[]
+        self.dp(0,s,[])
+        return self.lst
