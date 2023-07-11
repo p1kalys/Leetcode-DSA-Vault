08ae5d68accb12f139d54ab6e5192f3e4048e120
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-11 22:26:40 +0530

    Time: 50 ms (43.87%) | Memory: 16.4 MB (13.98%) - LeetSync

--- combination-sum-iii/combination-sum-iii.py
+++ combination-sum-iii/combination-sum-iii.py
@@ -1,14 +1,14 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         def dfs(i, cur):            
-            if sum(cur)  > n  or len(cur)  > k: # sum or length exceeded --> exit    
+            if sum(cur)  > n  or len(cur)  > k: 
                 return
-            if sum(cur) == n and len(cur) == k: # we found solution      --> add to result's array
+            if sum(cur) == n and len(cur) == k:
                 res.append(cur)
                 return
-            for j in range(i, len(nums)):       # we iterate from the left to the right             
-                dfs(j + 1, cur + [nums[j]])          # next element will be bigger (j + 1) than current
+            for j in range(i, len(nums)):     
+                dfs(j + 1, cur + [nums[j]])         
         res    = []
-        nums   = [i   for i in range(1, 10)]   # create array [1,2,3,4,5,6,7,8,9]
+        nums   = [i   for i in range(1, 10)] 
         dfs(0, [])
         return res
