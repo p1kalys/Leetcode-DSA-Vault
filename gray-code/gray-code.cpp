82c242fcab2a4928e51d69a1ca4adb45171b87c0
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-12 18:13:30 +0530

    Time: 65 ms (11.16%) | Memory: 39.8 MB (7.90%) - LeetSync

--- gray-code/gray-code.cpp
+++ gray-code/gray-code.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> res;
-        res.push_back( 0 );
-        for( int k = 0; k<n; k++ ) {
-            int i = res.size(), t = (1<<k) ;
-            while( i ) {
-                int temp = res[--i] | t;
-                res.push_back( temp );
-            }
+        vector<string>graystring = generate(n);
+    
+        for( auto i:graystring){
+            res.push_back(stoi(i,0,2));
         }
         return res;    
+    }
+    vector<string> generate(int n){
+        if (n==1){
+            return {"0","1"};
+        }
+        vector<string>ans;
+        vector<string>temp = generate(n-1);
+        for(int i=0; i<temp.size(); i++){
+            ans.push_back("0"+temp[i]);
+        }
+        for(int i=temp.size()-1; i>=0; i--){
+            ans.push_back("1"+temp[i]);
+        }
+        return ans;
     }
 };
