6e160cb5cdaad6e33bb6076cbb82d21e122eba2f
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-13 18:34:34 +0530

    Time: 51 ms (36.52%) | Memory: 16.1 MB (97.32%) - LeetSync

--- unique-paths/unique-paths.py
+++ unique-paths/unique-paths.py
@@ -4,8 +4,8 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if i == 0 or j == 0:
-                    dp[i][j] = 1
+                    dp[i][j] = 1 #base case
                 else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]#left+up
         
         return dp[m-1][n-1]
