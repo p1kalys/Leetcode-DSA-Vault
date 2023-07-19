6434ab400ef8690270c003a917a716de8d99d1f6
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-19 19:37:04 +0530

    Time: 7 ms (99.68%) | Memory: 11 MB (96.37%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        map<int,int>mp;
-        for(auto i:nums){
-            mp[i]++;
-        }
+        sort(nums.begin(),nums.end());
+        int i=0,j=1;
         int res=0;
-        for(auto i:mp){
-            if(k==0){
-                if(i.second>1){
-                    res++;
+        while (i<nums.size() && j<nums.size()){
+            if (i==j or nums[j]-nums[i]<k){
+                j++;
+            }
+            else if(nums[j]-nums[i]>k){
+                i++;
+            }
+            else{
+                res++;
+                i++;
+                while(i<nums.size() && nums[i]==nums[i-1]){
+                    i++;
                 }
             }
-            else if(k>0 && mp.find(k+i.first)!=mp.end()){
-                res++;
-            }
-
         }
         return res;
     }
