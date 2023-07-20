6e658e4a8a2e30b861598a183bc9a29fde593b43
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-20 19:41:04 +0530

    Time: 517 ms (20.53%) | Memory: 17.8 MB (80.64%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calculateTotalHours(self,v, hourly):
+        totalH = 0
+        n = len(v)
+        # Find total hours
+        for i in range(n):
+            totalH += math.ceil(v[i] / hourly)
+        return totalH
+        
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low = 1
+        high = max(piles)
+
+        # Apply binary search
+        while low <= high:
+            mid = (low + high) // 2
+            totalH = self.calculateTotalHours(piles, mid)
+            if totalH <= h:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
