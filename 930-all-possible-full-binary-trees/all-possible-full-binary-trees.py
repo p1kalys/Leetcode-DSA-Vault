c31c2c178120ee26405a7edf9602c489e7229a06
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-23 21:53:12 +0530

    Time: 188 ms (49.60%) | Memory: 20.6 MB (59.64%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -1,4 +1,13 @@
 class Solution:
     @cache
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        return [TreeNode(0, left, right) for k in range(1, n-1, 2) for left in self.allPossibleFBT(k) for right in self.allPossibleFBT(n-1-k)] if n != 1 else [TreeNode()]
+        if n == 1:
+            return [TreeNode()]
+        
+        result = []
+        for k in range(1, n-1, 2):
+            for left in self.allPossibleFBT(k):
+                for right in self.allPossibleFBT(n-1-k):
+                    result.append(TreeNode(0, left, right))
+        
+        return result
