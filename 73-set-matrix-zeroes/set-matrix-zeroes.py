1026445233b15c4f7384f49e6a01cc75c68ef35d
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-07-27 11:57:17 +0530

    Time: 142 ms (51.24%) | Memory: 17.3 MB (21.21%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row=[]
+        col=[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                    if matrix[i][j]==0:
+                        row.append(i)
+                        col.append(j)
+        for i in row:
+            for j in range(len(matrix[0])):
+                matrix[i][j]=0
+        for i in col:
+            for j in range(len(matrix)):
+                matrix[j][i]=0
