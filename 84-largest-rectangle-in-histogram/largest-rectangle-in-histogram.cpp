286a983dc380b20d4e9d16182ce565fed110905d
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-01 18:28:30 +0530

    Time: 131 ms (98.43%) | Memory: 76 MB (87.59%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -3,19 +3,26 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> s1; //initialise a stack
         int res = 0, n = heights.size();
-        
-        for(int i=0; i<=n; i++){
-            // check for prev and next smaller indices
-            while(!s1.empty() and (i==n || heights[s1.top()] >= heights[i])){
-                int len = heights[s1.top()];
+        int left[n],right[n];
+        for(int i=0; i<n; i++){
+            while(!s1.empty() and (heights[s1.top()] >= heights[i])){
                 s1.pop();
-                int bre;
-                if(s1.empty()) bre = i;
-                else bre = i - s1.top() - 1;
-                res = max(res, len*bre);
             }
-            
+            if(s1.empty())left[i]=0;
+            else left[i]=s1.top()+1;
             s1.push(i);
+        }
+        while (!s1.empty()) s1.pop();
+        for(int i=n-1; i>=0; i--){
+            while(!s1.empty() and (heights[s1.top()] >= heights[i])){
+                s1.pop();
+            }
+            if(s1.empty())right[i]=n-1;
+            else right[i]=s1.top()-1;
+            s1.push(i);
+        }
+        for(int i=0; i<n;i++){
+            res=max(res,heights[i]*(right[i]-left[i]+1));
         }
         return res;
     }
