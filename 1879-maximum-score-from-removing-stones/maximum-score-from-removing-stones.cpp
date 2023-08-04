95e56c155d078dd3f24f35054a51f4c1995df319
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-04 23:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (21.95%) - LeetSync

---
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        int maxi = max(a,max(b,c));
+        int mini = min(a,min(b,c));
+        int mid = a+b+c-maxi-mini;
+        if(mini+mid<maxi) return mini+mid;
+
+        return (a+b+c)/2;
+    }
+};
