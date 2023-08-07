dd876f3d4b3761a2cf65a55c03075e0a254d2f80
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-07 22:47:34 +0530

    Time: 56 ms (83.85%) | Memory: 18.9 MB (42.00%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        res=[]
+        def pre(root):
+            if not root:
+                return
+            res.append("(")
+            res.append(str(root.val))
+            if not root.left and root.right:
+                res.append("()")
+            pre(root.left)
+            pre(root.right)
+            res.append(")")
+        pre(root)
+        return "".join(res)[1:-1]
