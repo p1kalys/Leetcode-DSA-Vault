90a2388ecdf8c65264bd30b9263a48e27df8d095
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-11 19:27:10 +0530

    Time: 3060 ms (57.86%) | Memory: 17.7 MB (72.47%) - LeetSync

--- 2648-number-of-ways-to-earn-points/number-of-ways-to-earn-points.py
+++ 2648-number-of-ways-to-earn-points/number-of-ways-to-earn-points.py
@@ -1,22 +1,22 @@
 class Solution:    
-    def f(self, target,types,index,dp):
-        MOD=1e9+7
-        if (index==len(types)):
-            if target==0: 
+    def f(self, target, types, index, dp):
+        MOD = 1e9 + 7
+        if (index == len(types)):
+            if target == 0: 
                 return 1
             return 0
-        if dp[index][target]!=-1:
+        if dp[index][target] != -1:
             return dp[index][target]
-        ans=0
-        ans=(ans+self.f(target,types,index+1,dp)%MOD)%MOD
-        for j in range(1,types[index][0]+1):
-            if target>=j*types[index][1]:
-                ans=(ans+self.f(target-j*types[index][1],types,index+1,dp)%MOD)%MOD
+        ans = 0
+        ans = (ans + self.f(target, types, index + 1, dp) % MOD) % MOD
+        for j in range(1, types[index][0] + 1):
+            if target >= j * types[index][1]:
+                ans = (ans + self.f(target - j * types[index][1], types, index  + 1, dp) % MOD) % MOD
             else:
                 break
-        dp[index][target]=int(ans%MOD)
+        dp[index][target] = int(ans % MOD)
         return dp[index][target]
 
     def waysToReachTarget(self, target: int, types: List[List[int]]) -> int:
-        dp=[[-1]*(target+1) for _ in range(len(types))]
-        return self.f(target,types,0,dp)    
+        dp = [[-1] * (target + 1) for _ in range(len(types))]
+        return self.f(target, types, 0, dp)    
