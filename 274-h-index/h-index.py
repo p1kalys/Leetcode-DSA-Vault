7286b319bd9f4373d266947bf55e89e6573f88f5
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-12 12:51:24 +0530

    Time: 58 ms (31.55%) | Memory: 16.4 MB (98.80%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,13 +1,17 @@
 class Solution:
     def hIndex(self, c: List[int]) -> int:
-        c.sort(reverse=True)
-        #if len(c)==1 and c[0]>0:
-            #return 1
-        #if c[-1]>=len(c):#smallest number > element number
-            #return len(c)
-        h = 0
-        for i in range(len(c)):
-            if c[i]>= i+1: #i+1 is the length from 0 to i, if c[i] < the number of 
-            #elements >= c[i], then take i as the h index. Case[6,6,6,4,0],[0,0,2]
-                h= i+1
-        return h
+        c.sort()
+        n=len(c)
+        i = 0
+        j=n-1
+        while i<=j:
+            m=i+(j-i)//2
+            if c[m]==n-m:
+                return c[m]
+            elif c[m]>n-m:
+                j=m-1
+            else:
+                i=m+1
+        return n-i
+
+        
