d93db0e1c97f9b0d5eec07678ded82ca981e0ac4
p1kalys <85685112+p1kalys@users.noreply.github.com>
2023-08-12 23:55:42 +0530

    Time: 3 ms (88.85%) | Memory: 7.5 MB (98.81%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    bool help ( string s, int i , unordered_set<string>& words ,  vector<int>&dp ) 
-    {
-        if( i == s.size() ) return true ;
-        if( dp[i]!=-1 ) return dp[i];
-        string t = "";
-        bool ans = false ; 
-        for(int j = i ; j < s.size(); j ++ )
-        {
-            t += s[j];
-            if(words.find(t)!=words.end())
-            {
-                ans |= help( s , j + 1 , words,dp ) ;
-            }
-            if( ans ) return  dp[i] = ans ;
-        }
-        return dp[i] =  ans ; 
-    }
-   
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<int>dp(s.size(),-1);
-         unordered_set<string> dict(wordDict.begin(), wordDict.end());
-        return help(s,0,dict,dp) ; 
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+
+        for(int i = 0; i < s.size(); i++){
+            if(!dp[i]) continue;
+            for(auto word: wordDict){
+                if(s.substr(i, word.length()) == word && i + word.length() <= s.size()){
+                    dp[i + word.length()] = true;
+                }
+            }
+        }
+
+        return dp[s.size()];
     }
 };
