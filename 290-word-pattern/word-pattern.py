72aecefa286eedb6d7737d7b24e1cac6735b7f9d
Pavan Emani <pavanemani14@gmail.com>
2023-08-27 17:16:57 +0530

    Time: 41 ms (56.56%) | Memory: 16.3 MB (39.16%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,25 +1,17 @@
 class Solution(object):
     def wordPattern(self, pattern, s):
         l = s.split(" ")
-        dic = {}
+        d = {}
 
         #check if they are same in length
         if len(pattern) != len(l):
             return False
 
         for i in range(len(pattern)):
-            #add to dic if not exist
-            if pattern[i] not in dic.keys():
-                #add to dic if string's words do not exist
-                if l[i] not in dic.values():
-                    dic[pattern[i]] = l[i]
-                else:
+            if pattern[i] in d.keys() or l[i] in d.values():
+                if d.get(pattern[i]) != l[i]:
                     return False
             else:
-                #pass if they are the same, else break and return False
-                if dic[pattern[i]] == l[i]:
-                    continue
-                else:
-                    return False
-
+                d.update({pattern[i]: l[i]})
         return True
+        
