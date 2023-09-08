d5dd6a6ce130603031ddd2fbf05890ab1234c0b0
Pavan Emani <pavanemani14@gmail.com>
2023-09-08 20:06:16 +0530

    Time: 163 ms (98.71%) | Memory: 134.5 MB (92.49%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> res;
-        for(int i = 0; i < nums.size(); i++) {
-            // if the front element of the deque is the index i - k then its out of the window
-            // so we will have to pop the front most element
-            if (!dq.empty() && dq.front() == i - k) dq.pop_front();
-            // we are storing indices in dq.
-            // while nums[last_indice_stored] < nums[i], we will have to pop the last element
-            while (!dq.empty() && nums[dq.back()] < nums[i])
-            {
+        deque<int>dq;
+        vector<int>res;
+        for(int i=0; i<k; i++) {
+            while(!dq.empty() && nums[i]>nums[dq.back()]){
                 dq.pop_back();
             }
-            dq.push_back(i); // pushing index
-            if (i >= k - 1) res.push_back(nums[dq.front()]); // finding res array as ans
+            dq.push_back(i);
+        }
+        res.push_back(nums[dq.front()]);
+
+        for(int i=k;i<nums.size();i++){
+            if (!dq.empty() && dq.front()==i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[i]>=nums[dq.back()]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            res.push_back(nums[dq.front()]);
         }
         return res;
     }
