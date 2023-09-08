a3b7ccaabe1c98876d4571f8a049cb28854f89ed
Pavan Emani <pavanemani14@gmail.com>
2023-09-08 23:15:26 +0530

    Time: 128 ms (58.47%) | Memory: 18.7 MB (15.24%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,20 +1,20 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        L = []
+        L=[]
         for i in range(len(ranges)):
-            L.append([i - ranges[i], i + ranges[i]])
+            L.append([i-ranges[i],i+ranges[i]])
         L.sort()
-        i = 0
-        j = 0
-        count = 0
-        while i < n:
-            max = 0
-            while j < len(L) and L[j][0] <= i:
-                if L[j][1] > max:
-                    max = L[j][1]
-                j += 1
-            if max == 0:
+        i=0
+        j=0
+        count=0
+        while i<n:
+            maxi=0
+            while j<len(L) and L[j][0]<=i:
+                if L[j][1]>maxi:
+                    maxi=L[j][1]
+                j+=1
+            if maxi==0:
                 return -1
-            i = max
-            count += 1
+            i=maxi
+            count+=1 
         return count
