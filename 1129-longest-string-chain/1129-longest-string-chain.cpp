dbc68eb4909db5dea9fe4c1fd717a6b7232e9561
Pavan Emani <pavanemani14@gmail.com>
2023-09-23 14:26:45 +0530

    Time: 83 ms (66.29%), Space: 13.5 MB (59.76%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+bool check(string& s1, string& s2){
+
+    if(s1.size() != s2.size() + 1) return false;
+    
+    int first = 0;
+    int second = 0;
+    
+    while(first < s1.size()){
+        if(second < s2.size() && s1[first] == s2[second]){
+            first ++;
+            second ++;
+        }
+        else first ++;
+    }
+    if(first == s1.size() && second == s2.size()) return true;
+    else return false; 
+}
+
+    static bool comp(string& s1, string& s2){
+        return s1.size() < s2.size();
+    }
+
+    int longestStrChain(vector<string>& words) {   // based on LIS if observed carefully! 
+        
+        int n = words.size();
+        //sorting accordin to the size of each word using comparator!
+  //IMP -> SORT
+  sort( words.begin(), words.end(), comp );
+        vector<int> dp(n,1);
+        int maxi = 1;
+
+        for(int i=0; i<n; i++){
+            for(int prev=0; prev<i; prev++){
+
+                if(check(words[i],words[prev]) && dp[i]<dp[prev]+1){
+                    dp[i]=dp[prev]+1;
+                }
+            }
+            maxi = max(maxi,dp[i]);
+        }
+
+        return maxi;
+    }
+};
